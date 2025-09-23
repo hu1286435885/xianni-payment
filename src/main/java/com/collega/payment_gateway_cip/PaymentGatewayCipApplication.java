c537d2e0083e287b9c624fbc90a106126d1e5de7
farizibnu <fariz2240@gmail.com>
2025-09-23 12:17:58 +0700

    initial commit

---
+++ src/main/java/com/collega/payment_gateway_cip/PaymentGatewayCipApplication.java
@@ -0,0 +1,13 @@
+package com.collega.payment_gateway_cip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewayCipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGatewayCipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collega/payment_gateway_cip/PaymentGatewayCipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collega.payment_gateway_cip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayCipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
